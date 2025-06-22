0d9c470e556c93477e84ad43b6906d70e5e0c4e4
Atsuhito Machida <746984+neptaco@users.noreply.github.com>
2025-06-22 11:30:22 +0900

    revert: 確実に動作するバージョンにワークフローとフォーミュラを戻す

--- Formula/mcproc.rb
+++ Formula/mcproc.rb
@@ -3,16 +3,25 @@
 class Mcproc < Formula
   desc "Model Context Protocol server for AI agent process management"
   homepage "https://github.com/neptaco/mcproc"
-  url "https://github.com/neptaco/mcproc/releases/download/v#{version}/mcproc-#{version}-aarch64-apple-darwin.tar.gz"
   version "0.1.0"
-  sha256 "4bf2c1475ac03dacb1bdb0566f4b1563f7bd065257ef94e2b9285dab845c359b"
   license "MIT"
 
-  bottle do
-    sha256 arm64_apple: "4bf2c1475ac03dacb1bdb0566f4b1563f7bd065257ef94e2b9285dab845c359b"
-    sha256 x86_64_apple: "e8c135f0a313b9d4199ae0d520e2b663ea5c82feecca9a95bec1a1366ebe470e"
-    sha256 arm64_linux: "1901daa7d71990bc2294c009922b2b9ceef3215653c2f7600c5f9935c696e6ee"
-    sha256 x86_64_linux: "5a156e3bd7092e100d24fc8d87c7fb304fb8201df1920c1a95fedf5f865f4454"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/neptaco/mcproc/releases/download/v#{version}/mcproc-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "4bf2c1475ac03dacb1bdb0566f4b1563f7bd065257ef94e2b9285dab845c359b"
+    else
+      url "https://github.com/neptaco/mcproc/releases/download/v#{version}/mcproc-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "e8c135f0a313b9d4199ae0d520e2b663ea5c82feecca9a95bec1a1366ebe470e"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/neptaco/mcproc/releases/download/v#{version}/mcproc-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "1901daa7d71990bc2294c009922b2b9ceef3215653c2f7600c5f9935c696e6ee"
+    else
+      url "https://github.com/neptaco/mcproc/releases/download/v#{version}/mcproc-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "5a156e3bd7092e100d24fc8d87c7fb304fb8201df1920c1a95fedf5f865f4454"
+    end
   end
 
   def install
